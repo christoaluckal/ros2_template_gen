d5535f12333587be947f35815cc0f552226174ad
Christo Aluckal <christo12aluckal@gmail.com>
2024-07-08 19:14:25 -0400

    added better creation and templating

--- configure.py
+++ configure.py
@@ -4,16 +4,6 @@ import shutil
 ignore = [".git"]
 file_list = []
 
-for folder, subfolders, files in os.walk(os.getcwd()):
-    for file in files:
-        filePath = os.path.abspath(os.path.join(folder, file))
-        
-        # check if filepath has any of the ignore strings
-        if any([i in filePath for i in ignore]):
-            continue
-
-        file_list.append(filePath)
-        print(filePath)
 while True:
     package_name = input("Enter package name: ")
     base_py = input("Enter base python file name: ")
@@ -26,6 +16,26 @@ while True:
 
     break
 
+
+current_location = os.getcwd()
+up = os.path.abspath(os.path.join(current_location, os.pardir))
+os.chdir(up)
+shutil.copytree(os.path.join("ros2_py_template"), os.path.join(package_name))
+os.chdir(os.path.join(package_name))
+
+for folder, subfolders, files in os.walk(os.getcwd()):
+    for file in files:
+        filePath = os.path.abspath(os.path.join(folder, file))
+        
+        # check if filepath has any of the ignore strings
+        if any([i in filePath for i in ignore]):
+            continue
+
+        file_list.append(filePath)
+        print(filePath)
+
+
+
 for file in file_list:
     if "configure.py" in file:
         continue
@@ -34,6 +44,7 @@ for file in file_list:
         content = content.replace("__TEMPLATE_PACKAGE", package_name)
         content = content.replace("__TEMPLATE_EXEC_NAME", exec_name)
         content = content.replace("__TEMPLATE_NODE_NAME", node_name)
+        content = content.replace("__TEMPLATE_ENTRY", base_py)
 
     with open(file, "w") as f:
         f.write(content)
@@ -43,12 +54,7 @@ for file in file_list:
         shutil.move(file, new_file)
 
 shutil.move("__TEMPLATE_PACKAGE", package_name)
-
-
-current_location = os.getcwd()
-up = os.path.abspath(os.path.join(current_location, os.pardir))
-os.chdir(up)
-shutil.copytree(os.path.join("ros2_py_template"), os.path.join(package_name))
+shutil.move("resource/__TEMPLATE_PACKAGE", "resource/" + package_name)
 
     
     
--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from setuptools import find_packages, setup
+from glob import glob
 
 package_name = '__TEMPLATE_PACKAGE'
 
@@ -10,8 +11,8 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name + '/launch', ['launch/*.py']),
-        ('share/' + package_name + '/config', ['config/*.yaml']),
+        ('share/' + package_name + '/launch', glob('launch/*.py')),
+        ('share/' + package_name + '/config', glob('config/*.yaml'))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
